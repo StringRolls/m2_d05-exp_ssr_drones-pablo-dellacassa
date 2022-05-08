877f90ed3f2d69cc470993d5aa37649bc8b671e6
Pablo Dellacassa <pdellacassa@gmail.com>
2022-05-08 22:32:36 +0200

    Done, missing styles

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,14 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const droneSchema = new Schema({
+  // unless you are defining more than the "type" property, you don't have to use {} e.g.:
+  // firstName: {type: String, require: true}
+  name: String,
+  propellers: Number,
+  maxSpeed: Number,
+});
+
+module.exports = model('Drone', droneSchema);
